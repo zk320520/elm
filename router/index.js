cf2c9d4833741b21bc7dd4fab117c15927df60a8
Apple <zk320520@163.com>
2017-07-05 23:38:48 +0800

    数据库测试页面

--- router/index.js
+++ router/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/index');
 
-router.get('/user', (req, res) => {
-    User.save({name: req.query.name, password: req.query.password}, function(err) {
+router.post('/user', (req, res) => {
+    console.log(req);
+    User.save({name: req.body.name, password: req.body.password}, function(err) {
         if (err) {
             res.json(err);
         } else {
--- src/components/example.vue
+++ src/components/example.vue
@@ -1,9 +1,50 @@
 <template>
-    <h2>example</h2>
+    <div class="example">
+       <el-form ref="form" :model="form">
+            <el-form-item label="用户名:">
+                <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="密码:">
+                <el-input type="password" v-model="form.password"></el-input>
+            </el-form-item>
+            </el-form-item><el-form-item>
+                <el-button type="primary" @click="onSubmit()">登录</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
-        name: 'example'
+        name: 'example',
+        data() {
+            return {
+                form : {
+                    name: '',
+                    password: ''
+                } 
+            }
+        },   
+        methods: {
+            onSubmit: function() {
+                let url = 'api/user';
+                let name = this.name;
+                axios.post(url, {
+                    'name': this.form.name,
+                    'password': this.form.password
+                }).then(res => {
+                    console.log(res);
+                }).catch(err => {
+                    console.log(err);
+                });
+            }
+        }
     }
 </script>
+
+<style>
+    .example {
+
+    }
+</style>
